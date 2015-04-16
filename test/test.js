79dbe30676eb9ede3b7cc2ccb7b3c2a43e1d62f3
Munawwar Firoz <munawwarfiroz@hotmail.com>
2015-04-17 01:38:44 +0400

    Added element and text node test.

--- test/test.js
+++ test/test.js
@@ -6,14 +6,33 @@ var assert = require("assert"),
     jsdom = require('jsdom'),
     HTMLtoDOM = factory(jsdom.jsdom('').parentWindow);
 
-describe('run HTMLtoDOM test', function () {
-    var html = fetch('test/test.html'),
+describe('run consolidated HTMLtoDOM test', function () {
+    var html = fetch('test/multiple.html'),
         doc = HTMLtoDOM(html);
     it('it should have two nodes - doctype and html tag', function () {
         assert.equal(2, doc.childNodes.length);
     });
     it('html tag should have two child elements - head and body tag', function () {
         assert.equal(2, doc.childNodes[1].children.length);
+    });
+});
+
+describe('run element and text test', function () {
+    var html = fetch('test/element-and-text.html'),
+        df = HTMLtoDOM(html);
+    it('it should have exactly one tag', function () {
+        assert.equal(1, df.childNodes.length);
+    });
+    it('it should be a div', function () {
+        assert.equal('DIV', df.firstChild.nodeName);
+    });
+    it('class attribute of div should be "blah"', function () {
+        assert.equal('blah', df.firstChild.getAttribute('class'));
+    });
+    it('div tag should have one text node inside with text "Test"', function () {
+        assert.equal(1, df.firstChild.childNodes.length);
+        assert.equal(3, df.firstChild.firstChild.nodeType);
+        assert.equal('Test', df.firstChild.firstChild.nodeValue);
     });
 });
 
