549bead5a476ee894090847924a7592fa021b9cf
Munawwar Firoz <munawwarfiroz@hotmail.com>
2015-04-20 23:40:48 +0400

    Updated tests. Test taken from htmlparser2 (with the same numbering).

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ var assert = require("assert"),
     jsdom = require('jsdom'),
     HTMLtoDOM = factory(jsdom.jsdom('').parentWindow);
 
-describe('run consolidated HTMLtoDOM test', function () {
+describe('run consolidated HTMLtoDOM test - multiple.html', function () {
     var html = fetch('test/multiple.html'),
         doc = HTMLtoDOM(html);
     it('it should have two nodes - doctype and html tag', function () {
@@ -17,8 +17,8 @@ describe('run consolidated HTMLtoDOM test', function () {
     });
 });
 
-describe('run element and text test', function () {
-    var html = fetch('test/element-and-text.html'),
+describe('run element and text test - 01-simple.html', function () {
+    var html = fetch('test/01-simple.html'),
         df = HTMLtoDOM(html);
     it('it should have exactly one tag', function () {
         assert.equal(1, df.childNodes.length);
@@ -33,6 +33,34 @@ describe('run element and text test', function () {
         assert.equal(1, df.firstChild.childNodes.length);
         assert.equal(3, df.firstChild.firstChild.nodeType);
         assert.equal('Test', df.firstChild.firstChild.nodeValue);
+    });
+});
+
+describe('run self-closing tag test - 07-self-closing.html', function () {
+    var html = fetch('test/07-self-closing.html'),
+        df = HTMLtoDOM(html);
+    it('root should have exactly 2 immediate children', function () {
+        assert.equal(2, df.childNodes.length);
+    });
+    it('first child should be a anchor tag', function () {
+        assert.equal('A', df.firstChild.nodeName);
+    });
+    it('anchor tag should have child text node with "Foo" as it\'s value', function () {
+        assert.equal('Foo', df.firstChild.firstChild.nodeValue);
+    });
+    it('second child should be a hr tag', function () {
+        assert.equal('HR', df.childNodes[1].nodeName);
+    });
+});
+
+describe('run isolated less than angle bracket - 15-lt-whitespace.html', function () {
+    var html = fetch('test/15-lt-whitespace.html'),
+        df = HTMLtoDOM(html);
+    it('it should have exactly one child', function () {
+        assert.equal(1, df.childNodes.length);
+    });
+    it('it should be a text node with "a < b"', function () {
+        assert.equal('a < b', df.firstChild.nodeValue);
     });
 });
 
